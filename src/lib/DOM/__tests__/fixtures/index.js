66a77ef2e0efcf943efb3455d1122fd1d7da07ce
Gabin <hello@gabinaureche.com>
2017-03-30 23:20:54 +0200

    restructure project

--- src/lib/DOM/__tests__/fixtures/index.js
+++ src/lib/DOM/__tests__/fixtures/index.js
@@ -2,17 +2,11 @@
 
 const fs = require('fs');
 const path = require('path');
-
-function getHtml() {
-  return fs.readFileSync(path.join(__dirname, './graph.html'), 'utf8');
-}
-
-function getGraph() {
-  return window.document.querySelector('.js-calendar-graph-svg');
-}
+const html = fs.readFileSync(path.join(__dirname, './graph.html'), 'utf8');
 
 function getCell([x, y]) {
-  const cols = getGraph().querySelector('g').querySelectorAll('g');
+  const graph = window.document.querySelector('.js-calendar-graph-svg');
+  const cols = graph.querySelector('g').querySelectorAll('g');
   return cols[x].children[y];
 }
 
@@ -21,7 +15,7 @@ function getCellColor([x, y]) {
 }
 
 function resetHtml() {
-  window.document.body.innerHTML = getHtml();
+  window.document.body.innerHTML = html;
 }
 
 module.exports = {
