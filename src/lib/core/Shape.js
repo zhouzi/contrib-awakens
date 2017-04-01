520f4eece206d877c9c8c320966b32cfa55cae4a
Gabin <hello@gabinaureche.com>
2017-04-01 10:18:54 +0200

    simplify Shape

--- src/lib/core/Shape.js
+++ src/lib/core/Shape.js
@@ -1,35 +1,26 @@
 import uniqueId from 'lodash/uniqueId';
 import assign from 'lodash/assign';
 
-function addPoint(state, x, y, { id, name, color }) {
-  return assign(state, {
-    [`${x}.${y}`]: {
-      id,
-      name,
-      color,
-    },
-  });
-}
-
-function addPoints(id, name, y) {
-  return (state, color, x) => {
-    if (color == null) {
-      return state;
-    }
-
-    return addPoint(state, x, y, {
-      id,
-      name,
-      color,
-    });
-  };
+function getNonEmptyCells(cells) {
+  return cells
+    .map((color, x) => ({ color, x }))
+    .filter(({ color }) => color != null);
 }
 
 export default function Shape(name, shape) {
   const id = uniqueId(`${name}-`);
-  const points = shape.reduce((acc, cells, y) => (
-    cells.reduce(addPoints(id, name, y), acc)
-  ), {});
+  const points = shape
+    .map((cells, y) => (
+      getNonEmptyCells(cells)
+        .reduce((acc, { color, x }) => assign(acc, {
+          [`${x}.${y}`]: {
+            id,
+            name,
+            color,
+          },
+        }), {})
+    ))
+    .reduce((acc, cells) => assign(acc, cells), {});
 
   return {
     id,
