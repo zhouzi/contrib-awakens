ec99e48f8ee05716c06416f2d2dea5590475d7e2
Gabin <hello@gabinaureche.com>
2017-04-01 10:34:55 +0200

    use the minimum required properties from objects

--- src/lib/core/move.js
+++ src/lib/core/move.js
@@ -3,14 +3,16 @@ import omitBy from 'lodash/omitBy';
 import keys from 'lodash/keys';
 import has from 'lodash/has';
 import identity from 'lodash/identity';
-import position, { getNewPosition } from './position';
+import position, { getNewPointsPosition } from './position';
 
-function removeObject(state, { id }) {
+function removeObject(state, id) {
   return omitBy(state, point => point.id === id);
 }
 
-function getCollidingObjects(state, object) {
-  return keys(object.points)
+function getCollidingObjects(state, currentPoints, [x, y]) {
+  const newPoints = getNewPointsPosition(currentPoints, [x, y]);
+
+  return keys(newPoints)
     .map((coords) => {
       if (has(state, coords)) {
         return state[coords];
@@ -21,19 +23,20 @@ function getCollidingObjects(state, object) {
     .filter(identity);
 }
 
-export default function move(state, { id, name }, [incrementX, incrementY], callback = identity) {
-  const objectCurrentPosition = {
-    id,
-    name,
-    points: pickBy(state, obj => obj.id === id),
-  };
-  const objectNewPosition = getNewPosition(objectCurrentPosition, [incrementX, incrementY]);
-  const stateWithoutObject = removeObject(state, { id });
-  const collidingObjects = getCollidingObjects(stateWithoutObject, objectNewPosition);
+export default function move(state, { id }, [incrementX, incrementY], callback = identity) {
+  const objectCurrentPointsPosition = pickBy(state, obj => obj.id === id);
+  const stateWithoutObject = removeObject(state, id);
+  const collidingObjects = getCollidingObjects(
+    stateWithoutObject,
+    objectCurrentPointsPosition,
+    [incrementX, incrementY],
+  );
 
   if (collidingObjects.length) {
     return callback(state, collidingObjects);
   }
 
-  return position(stateWithoutObject, objectCurrentPosition, [incrementX, incrementY]);
+  return position(stateWithoutObject, {
+    points: objectCurrentPointsPosition,
+  }, [incrementX, incrementY]);
 }
--- src/lib/core/position.js
+++ src/lib/core/position.js
@@ -1,15 +1,13 @@
 import assign from 'lodash/assign';
 import mapKeys from 'lodash/mapKeys';
 
-export function getNewPosition(object, [newX, newY]) {
-  return assign({}, object, {
-    points: mapKeys(object.points, (value, key) => {
-      const [x, y] = key.split('.').map(Number);
-      return `${x + newX}.${y + newY}`;
-    }),
+export function getNewPointsPosition(points, [newX, newY]) {
+  return mapKeys(points, (value, key) => {
+    const [x, y] = key.split('.').map(Number);
+    return `${x + newX}.${y + newY}`;
   });
 }
 
-export default function position(state, car, [newX, newY]) {
-  return assign({}, state, getNewPosition(car, [newX, newY]).points);
+export default function position(state, { points }, [newX, newY]) {
+  return assign({}, state, getNewPointsPosition(points, [newX, newY]));
 }
