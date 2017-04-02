54a82025c67d654e48141f9b44028c6c2e78ee64
Gabin <hello@gabinaureche.com>
2017-04-02 15:17:48 +0200

    setup extension for random game selection

--- src/extension/content.js
+++ src/extension/content.js
@@ -1,12 +1,16 @@
 /* global window */
 
+import sample from 'lodash/sample';
 import createCarDodge from './games/carDodge';
 
-function startGame() {
-  createCarDodge(() => {
+const games = [
+  createCarDodge,
+];
+const createGame = sample(games);
+
+(function startGame() {
+  createGame(() => {
     window.alert('Game Over!');// eslint-disable-line no-alert
     startGame();
   });
-}
-
-startGame();
+}());
