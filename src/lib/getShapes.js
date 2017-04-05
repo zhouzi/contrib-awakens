9c3e481a824e6527898d93719237db56503f80cb
Gabin <hello@gabinaureche.com>
2017-04-05 13:56:08 +0200

    add getShape

--- src/lib/getShapes.js
+++ src/lib/getShapes.js
@@ -2,6 +2,11 @@ import reduce from 'lodash/reduce';
 import map from 'lodash/map';
 import merge from 'lodash/merge';
 import identity from 'lodash/identity';
+import pickBy from 'lodash/pickBy';
+
+export function getShape(state, id) {
+  return pickBy(state, point => point.id === id);
+}
 
 export default function getShapes(state) {
   const shapesMap = reduce(state, (acc, point, coord) => (
