9bd75482a162701a2fe3322c526b1226a5065bac
Gabin <hello@gabinaureche.com>
2017-04-13 21:55:10 +0200

    refactor crossRoad with pipeline

--- src/extension/games/crossRoad.js
+++ src/extension/games/crossRoad.js
@@ -14,6 +14,7 @@ import getInitialState, {
   reduceTop,
   reduceBottom,
   removeOutOfBoundsShapes,
+  pipeline,
   bounds,
   directions,
 } from '../../lib';
@@ -21,82 +22,86 @@ import throttle from '../../lib/throttle';
 import render, { loop, onKeyDown, keyCodes } from '../../lib/DOM';
 import colors from '../../lib/colors.json';
 
+const character = Shape('character', [
+  [colors.LIGHT],
+]);
+
+function moveCharacter(state, direction) {
+  const nextState = move(state, character, direction, () => null);
+  if (nextState == null) {
+    return null;
+  }
+
+  if (isOutOfBounds(nextState, character) === 0) {
+    return nextState;
+  }
+
+  return state;
+}
+
+function getRandomEmptyX(state) {
+  const nonEmptyX = getShapes(state)
+    .filter(shape => getShapeName(shape) === 'car')
+    .map(shape => keys(shape).map(coord => head(parseCoord(coord))))
+    .reduce((acc, x) => acc.concat(x), []);
+  const min = bounds.x.min + 3;
+  const max = bounds.x.max - 3;
+  const emptyX = range(min, max).filter(x => nonEmptyX.indexOf(x) === -1);
+  return sample(emptyX);
+}
+
+function moveCarsReducer(dir) {
+  return (acc, shape) => {
+    const name = getShapeName(shape);
+    const { direction } = getShapeMeta(shape);
+    if (name === 'car' && direction === dir) {
+      return move(acc, shape, directions[dir], () => null);
+    }
+
+    return acc;
+  };
+}
+
+function moveCarsWithSpeed(speed) {
+  return state => pipeline([
+    acc => reduceTop(acc, moveCarsReducer('BOTTOM', speed)),
+    acc => reduceBottom(acc, moveCarsReducer('TOP', speed)),
+  ], state);
+}
+
+function spawnCar(state) {
+  const x = getRandomEmptyX(state);
+  const direction = sample(['TOP', 'BOTTOM']);
+  const y = direction === 'BOTTOM' ? bounds.y.min - 1 : bounds.y.max;
+  const color = sample([colors.DARK, colors.DARKER]);
+  const speed = sample(['slow', 'fast']);
+  const car = Shape('car', [
+    [color],
+    [color],
+  ], { direction, speed });
+  return position(state, car, [x, y]);
+}
+
 export default function createCrossRoad() {
-  const character = Shape('character', [
-    [colors.LIGHT],
-  ]);
   let state = position(getInitialState(), character, [bounds.x.min + 1, bounds.y.middle]);
 
-  function moveCharacter(direction) {
-    const nextState = move(state, character, direction, () => null);
-    if (nextState == null) {
-      state = null;
-    } else if (isOutOfBounds(nextState, character) === 0) {
-      state = nextState;
-    }
-  }
-
-  const spawnCar = throttle(() => {
-    const nonEmptyX = getShapes(state)
-      .filter(shape => getShapeName(shape) === 'car')
-      .map(shape => keys(shape).map(coord => head(parseCoord(coord))))
-      .reduce((acc, x) => acc.concat(x), []);
-    const emptyX = range(bounds.x.min + 3, bounds.x.max - 3).filter(x => nonEmptyX.indexOf(x) === -1);
-    const x = sample(emptyX);
-    const direction = sample(['TOP', 'BOTTOM']);
-    const y = direction === 'BOTTOM' ? bounds.y.min - 1 : bounds.y.max;
-    const color = sample([colors.DARK, colors.DARKER]);
-    const speed = sample(['slow', 'fast']);
-    const car = Shape('car', [
-      [color],
-      [color],
-    ], { direction, speed });
-    state = position(state, car, [x, y]);
-  }, 500);
-  function moveCarsReducer(dir) {
-    return (state, shape) => {
-      const name = getShapeName(shape);
-      const { direction } = getShapeMeta(shape);
-      if (name === 'car' && direction === dir) {
-        return move(state, shape, directions[dir], () => null);
-      }
-
-      return state;
-    };
-  }
-  function moveCarsWithSpeed(speed) {
-    return () => {
-      state = reduceTop(state, moveCarsReducer('BOTTOM', speed));
-
-      if (state != null) {
-        state = reduceBottom(state, moveCarsReducer('TOP', speed));
-      }
-    };
-  }
   const moveSlowCars = throttle(moveCarsWithSpeed('slow'), 500);
   const moveFastCars = throttle(moveCarsWithSpeed('fast'), 250);
-
+  const throttledSpawnCar = throttle(spawnCar, 500);
   loop(() => {
-    if (state != null) {
-      moveSlowCars();
-    }
-
-    if (state != null) {
-      moveFastCars();
-    }
-
-    if (state != null) {
-      state = removeOutOfBoundsShapes(state);
-      spawnCar();
-    }
-
+    state = pipeline([
+      moveSlowCars,
+      moveFastCars,
+      removeOutOfBoundsShapes,
+      throttledSpawnCar,
+    ], state);
     render(state);
   });
 
   onKeyDown({
-    [keyCodes.TOP]: moveCharacter.bind(null, directions.TOP),
-    [keyCodes.RIGHT]: moveCharacter.bind(null, directions.RIGHT),
-    [keyCodes.BOTTOM]: moveCharacter.bind(null, directions.BOTTOM),
-    [keyCodes.LEFT]: moveCharacter.bind(null, directions.LEFT),
+    [keyCodes.TOP]: () => { state = moveCharacter(state, directions.TOP); },
+    [keyCodes.RIGHT]: () => { state = moveCharacter(state, directions.RIGHT); },
+    [keyCodes.BOTTOM]: () => { state = moveCharacter(state, directions.BOTTOM); },
+    [keyCodes.LEFT]: () => { state = moveCharacter(state, directions.LEFT); },
   });
 }
