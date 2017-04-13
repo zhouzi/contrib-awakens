edb70ec6d6423c8a56c85d88315bcd86cd4449c3
Gabin <hello@gabinaureche.com>
2017-04-13 21:17:32 +0200

    sometimes now returns first argument

--- src/lib/sometimes.js
+++ src/lib/sometimes.js
@@ -1,9 +1,11 @@
 import random from 'lodash/random';
+import head from 'lodash/head';
 
 export default function sometimes(fn, min, max) {
   return (...args) => {
     if (random(min, max) <= min) {
-      fn(...args);
+      return fn(...args);
     }
+    return head(args);
   };
 }
